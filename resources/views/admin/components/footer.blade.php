6adcecbc60499365ab95a48c9ef347110f33c5f7
vudevweb <vudo.contact.vn@gmail.com>
2024-10-06 16:27:40 +0700

    xóa hết

--- resources/views/admin/components/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2024 <a href="">{{ env('CMS_NAME') }}</a>.</strong>
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.0
-    </div>
-</footer>
--- resources/views/components/delete.blade.php
+++
@@ -1,4 +0,0 @@
-@props(['id'])
-<button  data-id="{{ $id }}" id="delete_tgnt" class="btn btn-danger btn-sm ">
-    <i class="bi bi-trash"></i>
-</button>

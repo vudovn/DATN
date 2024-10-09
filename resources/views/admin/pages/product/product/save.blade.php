245f6e62a4d070642d4c40498ab946bc00028b14
vudevweb <vudo.contact.vn@gmail.com>
2024-10-09 09:37:25 +0700

    FIX UI

--- resources/views/admin/pages/product/product/save.blade.php
+++ resources/views/admin/pages/product/product/save.blade.php
@@ -12,9 +12,7 @@
                     </div>
                     <div class="card-body">
                         <x-album :label="'Hình ảnh sản phẩm'" :name="'albums'" :value="$product->album ?? ''" />
-
                         <x-editor :label="'Mô tả sản phẩm'" :name="'description'" :value="$product->description ?? ''" class="form-control" />
-
                         {{-- SEO --}}
                         <x-seo :value_meta_title="$product->meta_title ?? ''" :value_meta_description="$product->meta_description ?? ''" :value_meta_keywords="$product->meta_keywords ?? ''" />
                     </div>
@@ -25,7 +23,7 @@
             <div class="col-lg-3 col-md-12 mb-4">
                 <x-save_back  :model="$config['model']"/>
                 <x-thumbnail :label="'Ảnh sản phẩm'" :name="'thumbnail'" :value="$product->thumbnail ?? '/uploads/system/no_img.jpg'" />
-                <x-publish :label="'Trạng thái'" :name="'publish'" :required="'true'" :value="$product->publish ?? ''" />
+                <x-publish :label="'Trạng thái'" :name="'publish'" :option="__('general.active')" :value="$product->publish ?? ''" />
             </div>
 
         </div>

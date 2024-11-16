a1b8e60fbc978f36dc92f4fdef29169fef161e71
aginCoder <phamgiaan545@gmail.com>
2024-11-16 17:57:56 +0700

    check

--- resources/views/client/pages/wishlist/wish_list.blade.php
+++ resources/views/client/pages/wishlist/wish_list.blade.php
@@ -13,45 +13,9 @@
         </div>
         <div class="wishlist-title">
             <h3>Mục yêu thích của bạn !</h3>
-            <p>7 sản phẩm</p>
+            <p>{{ $user->wishlists->count() }} sản phẩm</p>
         </div>
         <div class="wishlist-product row animate__animated animate__fadeIn">
-            <!-- 1 sản phẩm -->
-            {{-- <div class="col-md-6 col-lg-4 col-xl-3 col-6 d-flex justify-content-center mb-4">
-            <div class="card rounded overflow-hidden custom-card border-0">
-                <div class="position-relative" style="background-color: #EBEBEB;">
-                    <img src="\client_asset\image\product\wishlist3.png" class="card-img-top" alt="...">
-                    <div class="con-like position-absolute top-0 end-0 mt-2 me-3">
-                        <input class="like" type="checkbox" checked title="like">
-                        <div class="checkmark">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="outline" viewBox="0 0 24 24">
-                                <path
-                                    d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z">
-                                </path>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="filled" viewBox="0 0 24 24">
-                                <path
-                                    d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Z">
-                                </path>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="100" width="100" class="celebrate">
-                                <polygon class="poly" points="10,10 20,20"></polygon>
-                                <polygon class="poly" points="10,50 20,50"></polygon>
-                                <polygon class="poly" points="20,80 30,70"></polygon>
-                                <polygon class="poly" points="90,10 80,20"></polygon>
-                                <polygon class="poly" points="90,50 80,50"></polygon>
-                                <polygon class="poly" points="80,80 70,70"></polygon>
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body p-0 mt-2">
-                    <h5 class="card-text"><a href="">Ghế Sofa Gather</a></h5>
-                    <p class="card-text">120,000,000 đ</p>
-                </div>
-            </div>
-        </div> --}}
-            <!-- 1 sản phẩm -->
             <div class="wishlist-product row animate__animated animate__fadeIn">
                 @foreach ($user->wishlists as $wishlist)
                     <div class="col-md-6 col-lg-4 col-xl-3 col-6 d-flex justify-content-center mb-4 wishlist-item">

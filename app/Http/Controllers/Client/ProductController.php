e7651d0cebb1f707ec5e32d4d4db2747d00927af
vudevweb <vudo.contact.vn@gmail.com>
2024-11-17 18:19:11 +0700

    oke

--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -100,7 +100,8 @@ class ProductController extends Controller
                 // 'client_asset/custom/js/product/attribute_hex.min.js',
                 'client_asset/custom/js/addToCart.js',
                 'https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.min.js',
-        ];
+            ]
+            ];
     }
 
     private function breadcrumb()
